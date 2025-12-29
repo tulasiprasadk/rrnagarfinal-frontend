ba5538719016aae8ff6080e2eaf7f55af10d69e0
tulasiprasadk <tulasiprasadk@gmail.com>
2025-12-29 20:35:54 +0530

    Resolve merge conflict in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import axios from "axios";
 import { AuthProvider } from "./context/AuthContext";
-<<<<<<< HEAD
-=======
-import AuthProviderWrapper from "./auth0-provider.jsx";
->>>>>>> f97d87f8dce02737794187fb19e67f7d7827a941
 
 axios.defaults.withCredentials = true;
 const baseName = import.meta.env.VITE_BASE_URL || "/";
@@ -15,17 +11,9 @@ const baseName = import.meta.env.VITE_BASE_URL || "/";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-<<<<<<< HEAD
       <BrowserRouter basename={baseName}>
         <App />
       </BrowserRouter>
-=======
-      <AuthProviderWrapper>
-        <BrowserRouter basename={baseName}>
-          <App />
-        </BrowserRouter>
-      </AuthProviderWrapper>
->>>>>>> f97d87f8dce02737794187fb19e67f7d7827a941
     </AuthProvider>
   </React.StrictMode>
 );
